11cfe4fa09ac4bdc8674f161292017c6c7b03971
Akira Shinohara <34061817+s10akir@users.noreply.github.com>
2018-02-28 04:38:32 +0900

    [new] Tool class

---
+++ src/main/java/work/mojamoja/mineall/Tool.java
@@ -0,0 +1,36 @@
+package work.mojamoja.mineall;
+
+import org.bukkit.inventory.ItemStack;
+
+import static org.bukkit.Material.IRON_AXE;
+
+/**
+ * @author : akira.shinohara
+ * @since : 2018/02/28
+ */
+public class Tool {
+    public String getType(ItemStack tool) {
+        switch (tool.getType()) {
+            case WOOD_AXE:
+            case STONE_AXE:
+            case IRON_AXE:
+            case DIAMOND_AXE:
+                return "AXE";
+
+            case WOOD_SPADE:
+            case STONE_SPADE:
+            case IRON_SPADE:
+            case DIAMOND_SPADE:
+                return "SPADE";
+
+            case WOOD_PICKAXE:
+            case STONE_PICKAXE:
+            case IRON_PICKAXE:
+            case DIAMOND_PICKAXE:
+                return "PICKAXE";
+
+            default:
+                return "";
+        }
+    }
+}
